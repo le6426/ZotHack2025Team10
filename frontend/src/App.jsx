07e781dc138da21efb4e0efef776c17c54ffb7ba
Narin Park <narin@dhcp-172-31-170-204.mobile.uci.edu>
2025-11-08 11:42:38 -0800

    implemented routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
 import reactLogo from "./assets/react.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 import RandomItem from "@/components/RandomItem";
+import Home from "./components/home";
 
 /*
 This is the starting point of our application. Here, we can begin coding 
@@ -17,27 +19,13 @@ function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>PetrCAL</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-
-				<RandomItem maximum={1000} />
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more HI</p>
-		</>
-	);
+    <Router>
+		<Routes>
+			<Route path="/" element={<Home/>} />
+		</Routes>
+        {/* <Route path="/about" render={() => <AboutPage />} /> */}
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./home.css";
+
+import RandomItem from "@/components/RandomItem";
+
+function Home() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<div>
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank" rel="noreferrer">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>PetrCAL</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+
+				<RandomItem maximum={1000} />
+			</div>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more HI</p>
+		</>
+	);
+}
+
+export default Home;
