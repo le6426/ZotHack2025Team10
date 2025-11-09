1f0a50fdd9991ab5e69163bd30d089772b2094b0
Narin Park <narin@dhcp-172-31-170-204.mobile.uci.edu>
2025-11-08 16:53:46 -0800

    card creation

--- frontend/src/components/updates.jsx
+++ frontend/src/components/updates.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import "./updates.css";
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone'
+import { Calendar, dayjsLocalizer } from 'react-big-calendar'
 
+dayjs.extend(timezone)
+
+const localizer = dayjsLocalizer(dayjs)
 
 function Updates() {
 	const [count, setCount] = useState(0);
+
+    const myEventsList = [
+        { title: 'Team Meeting', start: new Date(2025, 10, 8, 9, 0), end: new Date(2025, 10, 8, 10, 0) },
+        { title: 'Project Deadline', start: new Date(2025, 10, 10, 0, 0), end: new Date(2025, 10, 10, 23, 59) },
+        { title: 'Lunch with Sarah', start: new Date(2025, 10, 12, 12, 30), end: new Date(2025, 10, 12, 13, 30) },
+        { title: 'Conference', start: new Date(2025, 10, 15, 9, 0), end: new Date(2025, 10, 17, 17, 0) },
+        { title: 'Yoga Class', start: new Date(2025, 10, 18, 18, 0), end: new Date(2025, 10, 18, 19, 0) }
+    ];
 
 	return (
 		<body id="updates-body">
@@ -20,6 +34,14 @@ function Updates() {
                 <button className="buttonstyle">THU</button>
                 <button className="buttonstyle">FRI</button>
 			</div>
+            {/* <div className="myCustomHeight">
+                <Calendar
+                    localizer={localizer}
+                    events={myEventsList}
+                    startAccessor="start"
+                    endAccessor="end"
+                />
+            </div> */}
 		</body>
 	);
 }
