9220dbd0cb8f7cb5251feed97edad32e4ac66f63
Nhan Le <le6426@gmail.com>
2025-11-08 21:03:32 -0800

    insta

---
+++ frontend/src/components/card.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect} from "react";
+import "./card.css";
+import { InstagramEmbed } from "react-social-media-embed";
+import { Link } from "react-router-dom";
+
+
+function Card({ petrs, time, title, url, createGoogleCalLink }) {
+
+    const date = new Date(time);
+    const formatter = new Intl.DateTimeFormat('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+    });
+    console.log(formatter.format(date)); // e.g., "14:30:45"  
+    console.log(petrs);
+
+    function toUTCString(dateObj) {
+        const year = dateObj.getUTCFullYear();
+        const month = String(dateObj.getUTCMonth() + 1).padStart(2, '0');
+        const day = String(dateObj.getUTCDate()).padStart(2, '0');
+        const hours = String(dateObj.getUTCHours()).padStart(2, '0');
+        const minutes = String(dateObj.getUTCMinutes()).padStart(2, '0');
+        const seconds = String(dateObj.getUTCSeconds()).padStart(2, '0');
+        // console.log("UTC String:", `${year}${month}${day}T${hours}${minutes}${seconds}Z`);
+        return `${year}${month}${day}T${hours}${minutes}${seconds}Z`;
+    }
+
+    const startUTC = toUTCString(date);
+
+    const endDate = new Date(date.getTime() + 60 * 30 * 1000);
+    const endUTC = toUTCString(endDate);
+
+      
+    // buttonAddToCalendar() {
+    //     // Logic to add event to calendar
+    //     <Link to={createGoogleCalLink(event)} target="_blank" rel="noopener noreferrer">
+    //         Add to Google Calendar
+    //     </Link>
+    // }
+
+
+	return (
+		<div className="card1">
+            <h3 className="title">{title}</h3>
+            <p>{petrs.join(", ")}</p>
+            <p>{formatter.format(date)}</p>
+            <InstagramEmbed url={url} width={300} captioned/>
+            <Link to={createGoogleCalLink(startUTC, endUTC, title, petrs)} target="_blank" rel="noopener noreferrer" className="calendar-link">
+                Add to Google Calendar
+            </Link>
+		</div>
+	);
+}
+
+export default Card;
--- frontend/src/components/updates.jsx
+++ frontend/src/components/updates.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import "./updates.css";
+import Card from "./card.jsx";
 import dayjs from 'dayjs'
 import timezone from 'dayjs/plugin/timezone'
 import { Calendar, dayjsLocalizer } from 'react-big-calendar'
@@ -9,6 +10,36 @@ dayjs.extend(timezone)
 const localizer = dayjsLocalizer(dayjs)
 
 function Updates() {
+    const [PetrList, setPetrList] = useState([]);
+    const fetchPetrs = async () => {
+        const response = await fetch("http://127.0.0.1:8000/api/petrdrops/heuyoClTLaIfcDZM8P2N/weekSix")
+        const PetrList = await response.json()
+        setPetrList(PetrList)
+    }
+
+    useEffect(() => {
+        fetchPetrs();
+    }, []);
+
+    console.log(PetrList);
+
+    function filterDayandSortbyTime(day) {
+        return PetrList.filter(petr => petr.day === day)
+            .sort((a, b) => new Date(a.time) - new Date(b.time));
+    }
+
+    const createGoogleCalLink = (time, end, title, petrs) => {
+        // const start = encodeURIComponent(time); // e.g. 20251108T180000
+        // const end = encodeURIComponent(end);
+        const details = encodeURIComponent(petrs.join(", "));
+        const text = encodeURIComponent(title);
+        // const location = encodeURIComponent(event.location);
+        console.log(' time:', time);
+      
+        return `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${text}&dates=${time}/${end}&details=${details}`;
+      };
+
+
 	const [count, setCount] = useState(0);
 
     const myEventsList = [
@@ -35,11 +66,41 @@ function Updates() {
                 <button className="buttonstyle">FRI</button>
 			</div>
             <div className="grid">
-                <div className="col"></div>
-                <div className="col"></div>
-                <div className="col"></div>
-                <div className="col"></div>
-                <div className="col"></div>
+                <div className="col">
+                    {filterDayandSortbyTime("Monday").map((petr) => (
+                        <div key={petr.id}>
+                            <Card petrs={petr.petrs} time={petr.time} title={petr.petrDrop} url={petr.url} createGoogleCalLink={createGoogleCalLink}/>
+                        </div>
+                    ))}
+                </div>
+                <div className="col">
+                    {filterDayandSortbyTime("Tuesday").map((petr) => (
+                        <div key={petr.id}>
+                            <Card petrs={petr.petrs} time={petr.time} title={petr.petrDrop} url={petr.url} createGoogleCalLink={createGoogleCalLink}/>
+                        </div>
+                    ))}
+                </div>
+                <div className="col">
+                    {filterDayandSortbyTime("Wednesday").map((petr) => (
+                        <div key={petr.id}>
+                            <Card petrs={petr.petrs} time={petr.time} title={petr.petrDrop} url={petr.url} createGoogleCalLink={createGoogleCalLink}/>
+                        </div>
+                    ))}
+                </div>
+                <div className="col">
+                    {filterDayandSortbyTime("Thursday").map((petr) => (
+                        <div key={petr.id}>
+                            <Card petrs={petr.petrs} time={petr.time} title={petr.petrDrop} url={petr.url} createGoogleCalLink={createGoogleCalLink}/>
+                        </div>
+                    ))}
+                </div>
+                <div className="col">
+                    {filterDayandSortbyTime("Friday").map((petr) => (
+                        <div key={petr.id}>
+                            <Card petrs={petr.petrs} time={petr.time} title={petr.petrDrop} url={petr.url} createGoogleCalLink={createGoogleCalLink}/>
+                        </div>
+                    ))}
+                </div>
             </div>
 		</body>
 	);
